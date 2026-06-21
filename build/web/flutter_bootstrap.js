6676e69254cde207b696b05028e8d2bd3259bfcc
percykipala60-hub <percykipala60@gmail.com>
2026-06-21 01:25:39 +0100

    Update web build with latest changes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1783057224" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2271555893" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
